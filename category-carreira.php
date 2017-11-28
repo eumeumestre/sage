61a74cc822a5efa7d9a90a338bd763b9b32699fb
eumeumestre <bruno@meumestre.eu>
2017-11-28 20:27:32 +0100

    new html for grids

--- category-carreira.php
+++ category-carreira.php
@@ -1,54 +1,32 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <!--START CARREIRA CONTENT-->
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350" />
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+<span>Destaque em</span>
+
+      <div class="row">
+
+        <div class="col-md-8">
+           <img src="http://via.placeholder.com/730x350">
+           <a>Lorem ipsum</a>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+        </div>
+
+        <div class="col-md-4">
+           <img src="http://via.placeholder.com/350x150">
+           <a>Lorem ipsum</a>
+           <img src="http://via.placeholder.com/350x150">
+           <a>Lorem ipsum</a>
          </div>
+
       </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
+
 <hr>
-<div class="row">
-   <div class="col-4">
-      <div>
-         <img src="http://via.placeholder.com/350x150" />
-         <a>Lorem ipsum</a>
+
+      <div class="row">
+        <div class="col-md-4"><img src="http://via.placeholder.com/350x150"> <a>Lorem ipsum</a></div>
+        <div class="col-md-4"><img src="http://via.placeholder.com/350x150"> <a>Lorem ipsum</a></div>
+        <div class="col-md-4"><img src="http://via.placeholder.com/350x150"> <a>Lorem ipsum</a></div>
       </div>
-   </div>
-   <div class="col-4">
-      <div>
-         <img src="http://via.placeholder.com/350x150" />
-         <a>Lorem ipsum</a>
-      </div>
-   </div>
-   <div class="col-4">
-      <div>
-         <img src="http://via.placeholder.com/350x150" />
-         <a>Lorem ipsum</a>
-      </div>
-   </div>
-</div>
 <!--END CARREIRA CONTENT-->
 
 <?php the_posts_navigation(); ?>
--- templates/page-home.php
+++ templates/page-home.php
@@ -1,250 +1,59 @@
 <div class="jumbotron">
-  <h1 class="display-3">Lorem Ipsum is simply dummy text of the printing</h1>
-  <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
-  <hr class="my-4">
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Lorem</a>
-  </p>
+   <h1 class="display-3">Lorem Ipsum is simply dummy text of the printing</h1>
+
+
+   <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
+
+   <hr class="my-4">
+
+
+   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</p>
+
+
+   <p class="lead"><a class="btn btn-primary btn-lg" href="#" role="button">Lorem</a>
+   </p>
 </div>
 
 <hr>
 
-<div class="row">
-    <div class="col-4">
-        <div>
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
+
+      <div class="row">
+        <div class="col-md-4">1</div>
+        <div class="col-md-4">2</div>
+        <div class="col-md-4">3</div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-md-4">4</div>
+        <div class="col-md-4">5</div>
+        <div class="col-md-4">6</div>
+      </div>
+
+
+<hr>
+
+
+
+     <span>Destaque em</span>
+
+      <div class="row">
+
+        <div class="col-md-8">
+           <img src="http://via.placeholder.com/730x350">
+           <a>Lorem ipsum</a>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
         </div>
-    </div>
-        <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-    </div>
-        <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150" />
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-    </div>
-</div>
 
-<hr>
-
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+        <div class="col-md-4">
+           <img src="http://via.placeholder.com/350x150">
+           <a>Lorem ipsum</a>
+           <img src="http://via.placeholder.com/350x150">
+           <a>Lorem ipsum</a>
          </div>
+
       </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
 
-<hr>
 
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
-         </div>
-      </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
 
-<hr>
 
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
-         </div>
-      </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
-
-<hr>
-
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
-         </div>
-      </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
-
-<hr>
-
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
-         </div>
-      </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
-
-<hr>
-
-<div class="row"><div class="col col-8"><p>Destaque em</p></div><div class="col col-4 text-right"><p>Ver todos em</p></div></div>
-<div class="row">
-   <div class="col-8">
-      <div>
-         <div>
-            <img src="http://via.placeholder.com/730x350">
-            <a>Lorem ipsum</a>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
-         </div>
-      </div>
-   </div>
-   <div class="col-4">
-      <row>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-         <div>
-            <div>
-               <img src="http://via.placeholder.com/350x150">
-               <a>Lorem ipsum</a>
-            </div>
-         </div>
-      </row>
-   </div>
-</div>
