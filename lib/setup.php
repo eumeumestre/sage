96b4ac0b82064a05ef9575b9f7db4c554763186a
eumeumestre <bruno@meumestre.eu>
2017-11-30 11:46:39 +0100

    manual codesnippet

--- lib/setup.php
+++ lib/setup.php
@@ -163,3 +163,42 @@ add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
 
 
+
+
+function wpb_postsbycategory() {
+// the query
+$the_query = new WP_Query( array( 'psicologia' => 'announcements', 'posts_per_page' => 10 ) ); 
+ 
+// The Loop
+if ( $the_query->have_posts() ) {
+    $string .= '<ul class="postsbycategory widget_recent_entries">';
+    while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+            if ( has_post_thumbnail() ) {
+            $string .= '<li>';
+            $string .= '<a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_post_thumbnail($post_id, array( 50, 50) ) . get_the_title() .'</a></li>';
+            } else { 
+            // if no featured image is found
+            $string .= '<li><a href="' . get_the_permalink() .'" rel="bookmark">' . get_the_title() .'</a></li>';
+            }
+            }
+    } else {
+    // no posts found
+}
+$string .= '</ul>';
+ 
+return $string;
+ 
+/* Restore original Post Data */
+wp_reset_postdata();
+}
+// Add a shortcode
+add_shortcode('categoryposts', 'wpb_postsbycategory');
+ 
+// Enable shortcodes in text widgets
+add_filter('widget_text', 'do_shortcode');
+
+
+
+
+
