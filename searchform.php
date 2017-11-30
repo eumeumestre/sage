71fe5ac1bd383fb5d51960e60938f8efaaa89d52
eumeumestre <bruno@meumestre.eu>
2017-11-30 18:51:21 +0100

    added search form to header form

---
+++ searchform.php
@@ -0,0 +1,7 @@
+					<form role="search" method="get" class="search-form form-inline my-2 my-lg-0" action="http://localhost:8080/">
+						<label id="email-label2">
+							<i class="fa fa-search"></i>
+							<input class="form-control mr-2" style="width: 195px;" placeholder="Pesquisar por" value="" name="s">
+							<button type="submit" class="btn btn-primary">Ir</button>
+						</label>
+					</form>
--- templates/header-relacionamentos.php
+++ templates/header-relacionamentos.php
@@ -63,13 +63,7 @@
         ?>
 
         <div class="emm-header-form">
-          <form class="form-inline my-2 my-lg-0">
-            <label id="email-label2">
-              <i class="fa fa-search"></i>
-              <input class="form-control mr-2" style="width: 195px;" placeholder="Pesquisar por">
-              <button type="submit" class="btn btn-primary">Ir</button>
-            </label>
-          </form>
+          <?php get_search_form(); ?>
         </div>
 
         <div class="emm-login" style="width: 32px;">
