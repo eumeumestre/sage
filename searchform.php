3b288990893e44a226613479d03fd3678c9a2e06
eumeumestre <bruno@meumestre.eu>
2017-12-01 19:38:36 +0100

    fixed searchform.php

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-					<form role="search" method="get" class="search-form form-inline my-2 my-lg-0" action="http://localhost:8080/">
+					<form role="search" method="get" class="search-form form-inline my-2 my-lg-0" action="http://www.meumestre.eu/">
 						<label id="email-label2">
 							<i class="fa fa-search"></i>
 							<input class="form-control mr-2" style="width: 195px;" placeholder="Pesquisar por" value="" name="s">
