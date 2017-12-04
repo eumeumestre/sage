1a0f0d74f659dbe343d9327478f9fe6580650ee9
eumeumestre <bruno@meumestre.eu>
2017-12-04 17:06:03 +0100

    readded newsletterbox

--- templates/content-newsletter-jumbo.php
+++ templates/content-newsletter-jumbo.php
@@ -9,12 +9,6 @@
 		<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
 		</div>
 	<div class="col-md-5">
-		<form class="form-inline">
-						    <input type="password" id="inputPassword6" class="form-control mr-2" aria-describedby="passwordHelpInline" placeholder="Insira seu e-mail">
-						    <small id="passwordHelpInline" class="text-muted">
-						    </small>
-						    <button type="submit" class="btn btn-primary">Inserir-me</button>
-						</form>
 							<?php echo do_shortcode("[gem id=1237789]"); ?>
 	</div>
 </div>
