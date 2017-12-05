5882f1360e9ced4b53a1aed39589cb906731e166
eumeumestre <bruno@meumestre.eu>
2017-12-05 16:27:58 +0100

    more texts and css

--- templates/page-home.php
+++ templates/page-home.php
@@ -15,10 +15,34 @@
   </div>
 </div>
 
-  <div class="mais-em-cat">
+
+<div class="mais-em-cat">
     <div class="row">
       <div class="col-md-8">
         <h3>Destaque em Psicologia</h3> <small>Lorem lorem ipsum lorem ipsum lorem.</small>
+      </div>
+      <div class="col-md-4 mais-em">
+        <a href="#">Mais em Psicologia <i aria-hidden="true" class="fa fa-long-arrow-right"></i></a>
+      </div>
+    </div><hr>
+
+  <div class="row">
+    <div class="col-md-8 featured-big">
+      <a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x350"><p>Lorem ipsum</p></a>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+    </div>
+    <div class="col-md-4 featured-small">
+      <a href="#"><img class="img-fluid" src="http://via.placeholder.com/750x350"><p>Lorem ipsum</p></a>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s</p>
+    </div>
+  </div>
+</div>
+
+
+  <div class="mais-em-cat">
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Destaque em Carreira</h3> <small>Idéias e aprendizados para maximizar sua performance profissional.</small>
       </div>
       <div class="col-md-4 mais-em">
         <a href="#">Mais em Psicologia <i aria-hidden="true" class="fa fa-long-arrow-right"></i></a>
