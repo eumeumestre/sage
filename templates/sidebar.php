5b1d4904a489a142985b13a24b226df8d00d61eb
eumeumestre <bruno@meumestre.eu>
2017-12-11 09:00:06 +0100

    removed sidebar newsletter box

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,5 @@
 <div><?php dynamic_sidebar('sidebar-primary'); ?></div>
+
+<div class="sidebar-ad">
+	<div><small>Publicidade</small></div><img class="img-fluid" src="http://via.placeholder.com/160x600">
+</div>
