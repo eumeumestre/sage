3b67a27b0ec4709a660fd80d5e64abf08cc88b25
eumeumestre <bruno@meumestre.eu>
2017-12-24 15:46:59 +0100

    changed newsletter link

--- templates/header.php
+++ templates/header.php
@@ -77,7 +77,7 @@
 				</div>
 
 				<div class="emm-premium">
-					<a href="/newsletter">
+					<a href="http://eepurl.com/dfd6t9" target="_blank">
 						<i class="fa fa-envelope-o" aria-hidden="true"></i><b>Assine a newsletter</b>
 					</a>
 				</div>
