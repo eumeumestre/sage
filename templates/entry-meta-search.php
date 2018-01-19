e78a7a9ccc1ee00100d479275e3c034a76a85be0
eumeumestre <bruno@meumestre.eu>
2018-01-19 20:59:17 -0200

    correct last commit icon font

--- templates/entry-meta-search.php
+++ templates/entry-meta-search.php
@@ -1,5 +1,5 @@
 <div style="margin-bottom: 5px; color: darkgrey;">
-<small><i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 4px;"></i><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
+<small><i class="fa fa-clock-o" aria-hidden="true" style="margin-right: 4px;"></i><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
 	<?= get_the_date(); ?>
 </time></small>
 </div>
