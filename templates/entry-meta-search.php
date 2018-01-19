445078b0547df7c23c52f42162f204589cdcda2f
eumeumestre <bruno@meumestre.eu>
2018-01-19 20:57:13 -0200

    font icons updates

--- templates/entry-meta-search.php
+++ templates/entry-meta-search.php
@@ -1,8 +1,5 @@
 <div style="margin-bottom: 5px; color: darkgrey;">
-<small><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
+<small><i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 4px;"></i><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
 	<?= get_the_date(); ?>
 </time></small>
-	<small>por <span class="fn"><?= get_the_author(); ?></span></small>
-<br>
-<small>Autor original: <?php echo get_post_meta($post->ID, 'Autor', true); ?></small>
 </div>
--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,10 +1,9 @@
 <div style="margin-bottom: 20px; color: darkgrey;">
-<small><i class="fa fa-clock-o" aria-hidden="true"></i><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
+<small><i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 4px;"></i><span>Publicado em</span><time class="updated" datetime="<?= get_post_time('c', true); ?>">
 	<?= get_the_date(); ?>
 </time></small>
-	<small>por <span class="fn"><?= get_the_author(); ?></span></small>
 <br>
-
-<small>Autor original: <?php echo get_post_meta($post->ID, 'Autor', true); ?></small> - 
-<small>Traduzido e adaptado de: <?php echo get_post_meta($post->ID, 'Fonte', true); ?></small>
+<small><i class="fa user-o" aria-hidden="true" style="margin-left: 4px;"></i>Autor original: <?php echo get_post_meta($post->ID, 'Autor', true); ?></small>
+<br>
+<small><i class="fa fa-external-link" aria-hidden="true" style="margin-left: 4px;"></i>Traduzido e adaptado de: <?php echo get_post_meta($post->ID, 'Fonte', true); ?></small>
 </div>
