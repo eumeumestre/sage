a87035feeecc3871b88218aca9f95c2da2bce8e3
eumeumestre <bruno@meumestre.eu>
2018-01-21 17:38:07 -0200

    remove ad from sidebar template

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,5 +1,1 @@
 <div><?php dynamic_sidebar('sidebar-primary'); ?></div>
-
-<div class="sidebar-ad">
-	<div><small>Publicidade</small></div><a href="http://www.meumestre.eu/anuncie"><img class="img-fluid" src="http://via.placeholder.com/160x600?text=Anuncie%20aqui"></a>
-       </div>
