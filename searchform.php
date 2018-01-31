bb4bc38f283860d6eef84f5fb4b2f3081f76bf24
eumeumestre <bruno@meumestre.eu>
2018-01-31 16:52:51 -0200

    new search form

--- searchform.php
+++ searchform.php
@@ -1,7 +1,6 @@
-<form role="search" method="get" class="search-form form-inline my-2 my-lg-0" action="http://www.meumestre.eu/">
-    <label id="email-label2">
+<form role="search" method="get" class="" action="http://www.meumestre.eu/">
         <i class="fa fa-search"></i>
-        <input class="form-control mr-2" style="width: 195px;" placeholder="Pesquisar por" value="" name="s">
-        <button type="submit" class="btn btn-secondary">Ir</button>
-    </label>
+        <input type="search" class="form-control mr-2" placeholder="Pesquisar por" value="" name="s">
+        <button type="submit" class="btn btn-secondary d-none">Ir</button>
 </form>
+
