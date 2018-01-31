321d3b1d631c99b35034575b7d5e9b0f62558c39
eumeumestre <bruno@meumestre.eu>
2018-01-31 17:38:41 -0200

    searchh

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form role="search" method="get" class="" action="http://www.meumestre.eu/">
-        <i class="fa fa-search"></i>
         <input type="search" class="form-control mr-2" placeholder="Pesquisar por" value="" name="s">
         <button type="submit" class="btn btn-secondary d-none">Ir</button>
 </form>
