8db22a166218be7979d2ef4612e2bac1ebdbec17
eumeumestre <bruno@meumestre.eu>
2018-02-05 14:06:11 -0200

    adsense code

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,13 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+
+<script async src="//"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3870151252326045",
+    enable_page_level_ads: true
+  });
+</script>
+
 </head>
