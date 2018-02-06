7f5a2c1b684cdbe19936a5c2efdc1f82ff865f87
eumeumestre <bruno@meumestre.eu>
2018-02-06 11:48:13 -0200

    adsense adjust

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,20 @@
-<div><?php dynamic_sidebar('sidebar-primary'); ?></div>
+<div><?php dynamic_sidebar('sidebar-primary'); ?>
+
+
+<div class="sidebar-ad">
+	<div><small>Publicidade</small></div>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Sidebar -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-3870151252326045"
+     data-ad-slot="9976504513"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+
+
+
+
+</div>
