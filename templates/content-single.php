ea9f432e00e524606cc054042a8cef754ae643ec
eumeumestre <bruno@meumestre.eu>
2018-02-06 12:03:09 -0200

    testing in article ads

--- templates/content-single.php
+++ templates/content-single.php
@@ -16,6 +16,18 @@
     
     <div class="entry-content">
       <?php the_content(); ?>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-3870151252326045"
+     data-ad-slot="8146926831"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
