1c20ae4c077340f6500b88b33280cb79a33d97d7
eumeumestre <bruno@meumestre.eu>
2018-02-08 11:03:11 -0200

    home back to code template

--- template-custom.php
+++ template-custom.php
@@ -5,4 +5,90 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
+  
+
+<div class="jumbotron"><div class="featured-label">Escolha do Editor</div>
+  <h2 class="display-5"><a href="/carreira/o-que-fazer-quando-paixao-pelo-trabalho-nao-e-o-suficiente/">O que fazer quando a paixão pelo trabalho<br> não é o suficiente</a></h2>
+  <p class="lead">Se chama trabalho por um motivo, e mesmo que você tenha sorte, estará sujeito a
+dias ruins. Essas dicas são para você que anda desmotivado ou pensando se é
+hora de mudar de ares.</p>
+  <p><a class="btn btn-primary btn-lg" href="/carreira/o-que-fazer-quando-paixao-pelo-trabalho-nao-e-o-suficiente/" role="button">Leia mais<i class="fa fa-chevron-circle-right" aria-hidden="true"></i></a></p>
+</div>
+<div class="artigos-destaque-home">
+  <div class="row">
+    <div class="col-md-4 featured-small"><a href="/psicologia/pare-de-se-comparar-com-outras-pessoas"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-1.jpg"><div class="featured-label">Destaque</div><h2>Pare de se comparar com outras pessoas</h2></a>
+    </div>
+    <div class="col-md-4 featured-small"><a href="/estilo-de-vida/abracando-a-simplicidade"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-2.jpg"><div class="featured-label">Destaque</div><h2>Abraçando a simplicidade</h2></a>
+    </div>
+    <div class="col-md-4 featured-small"><a href="/financas/os-principios-basicos-do-dinheiro"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-3.jpg"><div class="featured-label">Destaque</div><h2>Os princípios básicos do dinheiro</h2></a>
+    </div>
+  </div>
+</div>
+<div class="d-none" id="msgid">
+</div><a class="d-none" href="#" data-toggle="tooltip" title="Hooray!">Hover over me</a>
+<div class="mais-em-cat">
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Destaque em Psicologia</h3> <small>Conceitos modernos da psicologia para exercitar a sua saúde mental.</small>
+      </div>
+      <div class="col-md-4 mais-em">
+        <a href="/psicologia">Mais em Psicologia <i aria-hidden="true" class="fa fa-long-arrow-right"></i></a>
+      </div>
+    </div><hr>
+
+  <div class="row">
+    <div class="col-md-8 featured-big">
+      <a href="/psicologia/os-habitos-da-felicidade"><img src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-big-4.jpg" class="img-fluid"><h2>Os hábitos da felicidade</h2></a>
+      <p>Manter o sentimento de felicidade não é fácil, mas investir nos hábitos certos é
+recompensador. Adotá-los fará uma grande diferença no seu humor e o
+aproximará de pessoas com a mesma mentalidade.</p>
+    </div>
+    <div class="col-md-4 featured-small">
+      <a href="/psicologia/inteligencia-emocional-o-verdadeiro-sucesso"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-5.jpg"><h2>Inteligência Emocional: o verdadeiro sucesso</h2></a>
+      <p>Durante anos, o teste de QI era o único instrumento de medição da inteligência com base em desempenho cognitivo. Entretanto, seria injusto se não considerássemos outras habilidades na balança final.</p>
+    </div>
+  </div>
+</div>
+<div class="mais-em-cat">
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Destaque em Saúde</h3> <small></small>
+      </div>
+      <div class="col-md-4 mais-em">
+        <a href="/saude">Mais em Saúde <i aria-hidden="true" class="fa fa-long-arrow-right"></i></a>
+      </div>
+    </div><hr>
+
+  <div class="row">
+    <div class="col-md-8 featured-big">
+      <a href="/saude/7-habitos-para-dias-mais-leves""><img src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-big-saude.jpg" class="img-fluid"><h2>7 hábitos para dias mais leves</h2></a>
+      <p>Você pode começar o dia apertando a soneca e arrastando-se para fora da cama ou então iniciá-lo com o pé direito.</p>
+    </div>
+    <div class="col-md-4 featured-small">
+      <a href="/saude/8-beneficios-inesperados-da-sauna/"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-saude-2.jpg"><h2>8 benefícios inesperados da sauna</h2></a>
+      <p>As saunas são basicamente pequenas salas aquecidas, que podem ser secas ou molhadas. Neste artigo, analisamos alguns dos benefícios inesperados da sauna.</p>
+    </div>
+  </div>
+</div>
+  <div class="mais-em-cat">
+    <div class="row">
+      <div class="col-md-8">
+        <h3>Destaque em Carreira</h3> <small>Idéias e aprendizados para maximizar sua performance profissional.</small>
+      </div>
+      <div class="col-md-4 mais-em">
+        <a href="/carreira">Mais em Carreira <i aria-hidden="true" class="fa fa-long-arrow-right"></i></a>
+      </div>
+    </div><hr>
+  <div class="row">
+    <div class="col-md-8 featured-big">
+      <a href="/carreira/ser-produtivo-vs-ocupado"><img src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-big-6.jpg" class="img-fluid"><h2>Ser produtivo vs ocupado</h2></a>
+      <p>Nunca foi tão evidente a mudança dos paradigmas em relação ao tempo de trabalho para as novas gerações. Afinal, quais as diferenças entre pessoas ocupadas e produtivas?</p>
+    </div>
+    <div class="col-md-4 featured-small">
+      <a href="/carreira/dicas-para-vencer-o-cansaco-no-trabalho"><img class="img-fluid" src="http://www.meumestre.eu/wp-content/themes/sage/dist/images/featured-small-7.jpg"><h2>Dicas para vencer o cansaço no trabalho</h2></a>
+      <p>Muitas pessoas experimentam a fadiga ao longo do dia, e não importa o quão motivado ou envolvido você esteja, dormir determinadas horas por dia e fazer as pausas recomendadas podem não ser suficientes.</p>
+    </div>
+  </div>
+</div>
+  
 <?php endwhile; ?>
