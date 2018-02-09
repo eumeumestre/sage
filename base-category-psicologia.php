55be7fc7a6b0f49684ce9532459c201a84437522
eumeumestre <bruno@meumestre.eu>
2018-02-09 17:53:04 -0200

    lithe chamg

--- base-category-psicologia.php
+++ base-category-psicologia.php
@@ -49,7 +49,7 @@ use Roots\Sage\Wrapper;
         <h3>Mais Recentes</h3><hr>
 
             <ul>
-            <?php $the_query = new WP_Query( array( 'category_name' => 'psicologia', 'posts_per_page' => 5 ) );  ?>
+            <?php $the_query = new WP_Query( array( 'category_name' => 'psicologia', 'posts_per_page' => 3 ) );  ?>
             <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
             <li>
               <a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'custom-size-1' );  ?><p style="margin-top: 5px;"><?php the_title(); ?></p></a>
