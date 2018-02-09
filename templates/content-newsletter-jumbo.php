f0408c3a662980c18d49e51b10e01029f73e8a41
eumeumestre <bruno@meumestre.eu>
2018-02-09 19:46:44 -0200

    ad cat page

--- templates/content-newsletter-jumbo.php
+++ templates/content-newsletter-jumbo.php
@@ -2,7 +2,16 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<div><small>Publicidade</small></div><img src="http://via.placeholder.com/970x250">
+				<div><small>Publicidade</small></div>
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Category Page -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:970px;height:250px"
+     data-ad-client="ca-pub-3870151252326045"
+     data-ad-slot="4881575023"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 			</div>
 		</div>
 	</div>
