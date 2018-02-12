869bc2fdc448c93d6beb23710bf779f46344075f
eumeumestre <bruno@meumestre.eu>
2018-02-12 16:55:28 -0200

    bottom ad to all cat pages

--- base-category-financas.php
+++ base-category-financas.php
@@ -89,6 +89,32 @@ use Roots\Sage\Wrapper;
 
 </div>
 
+
+<!-- AD BOTTOM START -->
+
+<div class="newsletter-horizontal-box-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div><small>Publicidade</small></div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Bottom Responsive -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3870151252326045"
+     data-ad-slot="8593836740"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- END BOTTOM END -->
+
+
 <!-- END Ultimos Top Artigos -->
 
 
