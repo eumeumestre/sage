dc58bdadf84c9b4f7d4523251201741ef08107b5
eumeumestre <bruno@meumestre.eu>
2018-02-15 17:27:20 -0200

    comment user and link

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -3,7 +3,7 @@
 	<?= get_the_date(); ?>
 </time></small>
 <br>
-<small style="display: none;"><i class="fa fa-user-o" aria-hidden="true" style="margin-right: 4px;"></i>Autor original: <?php echo get_post_meta($post->ID, 'Autor', true); ?></small>
+<!-- <small style="display: none;"><i class="fa fa-user-o" aria-hidden="true" style="margin-right: 4px;"></i>Autor original: <?php echo get_post_meta($post->ID, 'Autor', true); ?></small> -->
 <br>
-<small style="display: none;"><i class="fa fa-external-link" aria-hidden="true" style="margin-right: 4px;"></i>Traduzido e adaptado de: <?php echo get_post_meta($post->ID, 'Fonte', true); ?></small>
+<!-- <small style="display: none;"><i class="fa fa-external-link" aria-hidden="true" style="margin-right: 4px;"></i>Traduzido e adaptado de: <?php echo get_post_meta($post->ID, 'Fonte', true); ?></small> -->
 </div>
