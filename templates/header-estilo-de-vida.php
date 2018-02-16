bae41663beb7d8f4f15b63808133495a4fce4d40
eumeumestre <bruno@meumestre.eu>
2018-02-16 15:30:12 -0200

    new top ad

--- templates/header-estilo-de-vida.php
+++ templates/header-estilo-de-vida.php
@@ -80,6 +80,26 @@
 </div> 
 </header>
 
+<div class="newsletter-horizontal-box-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div><small>Publicidade</small></div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Top Responsive -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3870151252326045"
+     data-ad-slot="1494935725"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <!--START PAGE HEADER-->
 
