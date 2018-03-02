acfe0396a9938ad5d687b0e72ec267dfafa19381
eumeumestre <bruno@meumestre.eu>
2018-03-02 18:04:59 -0300

    404 page change

--- 404.php
+++ 404.php
@@ -3,9 +3,9 @@
 </header>
 
 <div class="page-content">
-	<p>A página solicitada não foi encontrada.</p>
-	<div class="alert alert-warning">
-	  <p>Erro 404</p>
+	<div class="alert alert-warning" role="alert">
+	  <strong>Erro 404</strong> A página solicitada não foi encontrada.
 	</div>
 	<p><a href="http://www.meumestre.eu/">Voltar ao site</a></p>
 </div>
+
