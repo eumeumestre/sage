0f9d9d36ea66f12dd8eec2e3e4bd7050144ba5d3
eumeumestre <bruno@meumestre.eu>
2018-03-03 09:43:22 -0300

    search form validation and search icon img to assets folder

--- functions.php
+++ functions.php
@@ -30,6 +30,13 @@ unset($file, $filepath);
 
 ////////////////////////////////////////////////////////////////
 
+add_filter( 'posts_search', function( $search, \WP_Query $q )
+{
+    if( ! is_admin() && empty( $search ) && $q->is_search() && $q->is_main_query() )
+        $search .=" AND 0=1 ";
+
+    return $search;
+}, 10, 2 );
 
 
 // Exclude pages from WordPress Search
--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" method="get" class="" action="http://www.meumestre.eu/">
-	<input type="search" class="form-control mr-2" placeholder="Pesquisar por" value="" name="s">
-	<button type="submit" class="btn btn-secondary d-none">Procurar</button>
+	<input id="s" type="search" class="form-control mr-2" placeholder="Pesquisar por" value="" name="s">
+	<button id="go-search" type="submit" class="btn btn-secondary d-none">Procurar</button>
 </form>
