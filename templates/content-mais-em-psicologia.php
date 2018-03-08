8556bd0077b85e95836d2dbe58dd62a1087e62a2
eumeumestre <bruno@meumestre.eu>
2018-03-08 16:19:25 -0300

    fdfda

--- templates/content-mais-em-psicologia.php
+++ templates/content-mais-em-psicologia.php
@@ -9,37 +9,37 @@
     <div class="row">
       <div class="col-md-4">
         <div class="featured-card-item-small">  
-        <a href="/psicologia/3-habitos-dificeis-que-se-tornam-mais-faceis-com-o-tempo/"><img alt="3 hábitos difíceis que se tornam mais fáceis com o tempo" class="img-fluid" src="<?= get_template_directory_uri(); ?>/dist/images/featured/small/666.jpg"><h2>3 hábitos difíceis que se tornam mais fáceis com o tempo</h2></a>
-        </div>
-        <div class="featured-card-item-small d-none">
-        <a href="#"><img alt="" class="img-fluid" src="http://via.placeholder.com/350x170"><h2>Lorem ipsum</h2></a>
+        <a href="<?php the_permalink($post = 476) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
         <div class="featured-card-item-small">
-        <a href="/psicologia/como-fazer-seus-haters-gostarem-de-voce/"><img alt="Como fazer seus haters gostarem de você" class="img-fluid" src="<?= get_template_directory_uri(); ?>/dist/images/featured/small/508.jpg"><h2>Como fazer seus haters gostarem de você</h2></a>
+        <a href="<?php the_permalink($post = 284) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
+        </div>
+        <div class="featured-card-item-small">
+        <a href="<?php the_permalink($post = 139) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
       </div>
 
       <div class="col-md-4">
-        <div class="featured-card-item-small d-none">  
-        <a href="#"><img alt="" class="img-fluid" src="http://via.placeholder.com/350x170"><h2>Lorem ipsum</h2></a>
+        <div class="featured-card-item-small">  
+        <a href="<?php the_permalink($post = 146) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
         <div class="featured-card-item-small">
-        <a href="/psicologia/magica-de-medir-seu-humor-todos-os-dias/"><img alt="A mágica de medir seu humor todos os dias" class="img-fluid" src="<?= get_template_directory_uri(); ?>/dist/images/featured/small/476.jpg"><h2>A mágica de medir seu humor todos os dias</h2></a>
+        <a href="<?php the_permalink($post = 148) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
         <div class="featured-card-item-small">
-        <a href="/psicologia/7-licoes-cruciais-que-as-pessoas-aprendem-tarde-na-vida/"><img alt="7 lições cruciais que as pessoas aprendem tarde na vida" class="img-fluid" src="<?= get_template_directory_uri(); ?>/dist/images/featured/small/284.jpg"><h2>7 lições cruciais que as pessoas aprendem tarde na vida</h2></a>
+         <a href="<?php the_permalink($post = 499) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
       </div>
 
       <div class="col-md-4">
-        <div class="featured-card-item-small d-none">  
-        <a href="#"><img alt="" class="img-fluid" src="http://via.placeholder.com/350x170"><h2>Lorem ipsum</h2></a>
+        <div class="featured-card-item-small">  
+        <a href="<?php the_permalink($post = 666) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
         <div class="featured-card-item-small d-none">
-        <a href="#"><img alt="" class="img-fluid" src="http://via.placeholder.com/350x170"><h2>Lorem ipsum</h2></a>
+        <a href="<?php the_permalink($post = 109) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
         <div class="featured-card-item-small d-none">
-        <a href="#"><img alt="" class="img-fluid" src="http://via.placeholder.com/350x170"><h2>Lorem ipsum</h2></a>
+        <a href="<?php the_permalink($post = 109) ?>"><?php the_post_thumbnail('featured-small', array('class' => 'img-fluid')); ?><h2><?php the_title(); ?></h2></a>
         </div>
       </div>
     </div>
